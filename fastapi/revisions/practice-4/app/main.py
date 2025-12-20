971253fdf662555488ed1006e55cf41a0757ee84
“stiendra-nagesh” <sitendranagesh@gmail.com>
2025-12-20 18:34:16 +0530

    add requirements.txt

---
+++ fastapi/revisions/practice-4/app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:8001",
+    # "https://www.youtube.com",
+    "https://www.google.com",
+    # "*"
+]
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
+@app.get("/")
+def hello_world():
+    return {"message": "Hello World!"}
+
